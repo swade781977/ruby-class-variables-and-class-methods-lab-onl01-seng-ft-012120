19e81575f153ebc1b59b8936dd4b78250d986297
Chris Wade <swade781977@yahoo.com>
2020-01-30 10:57:16 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+class Song 
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre + genre
+    @@count += 1
+    @@artists << artist
+  end
+  attr_reader :name, :artist, :genre
+  def name=(name)
+    @name = name
+    
+  end
+  def artist(artist)
+    @@artists[@@artists.index(@artist)] = artist
+    @artist = artist
+  end
+  def genre(genre)
+    @@genres[@@genres.index(@genre)] = genre
+    @genre = genre
+  end
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      
+    end
+  end
+  def self.artist_count
+    artist_count ={}
+    @@artists.each do |artist|
+      artist_count[artist] = 1 ||= artist_count[genre] += 1 
+    end
+  end
+  def self.count
+    @@count
+  end
+end
