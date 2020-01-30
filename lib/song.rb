cf40155f3889b405f9d55885a3a080c768093837
Chris Wade <swade781977@yahoo.com>
2020-01-30 11:13:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
-    @@genre << genre
+    @@genres << genre
     @@count += 1
     @@artists << artist
   end
